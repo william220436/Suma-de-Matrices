698a2d2e7b6bd1e093f8b00a85ee9ab83ce5dd04
william220436 <32716307+william220436@users.noreply.github.com>
2017-10-24 13:29:58 -0500

    Suma de Matrices

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int A[3][3], B[3][3], C[3][3];
+	printf("------------Matriz A------------\n");
+	for (int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			printf("Dame un numero: ");
+			scanf("%i", &A[i][j]);
+		}
+	}
+	for (int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			printf("%i ", A[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("------------Matriz B------------\n");
+	for (int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			printf("Dame un numero: ");
+			scanf("%i", &B[i][j]);
+		}
+	}	
+
+	for (int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			printf("%i ", B[i][j]);
+		}
+		printf("\n");
+	}	
+	printf("\n");
+	
+	
+	
+	
+	return 0;
+}
